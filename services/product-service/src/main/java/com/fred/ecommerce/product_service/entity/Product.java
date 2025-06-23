b6690faf8a8838b9a69da255f1e1bb1130e7df76
Frederico Costa <Fredericobrasilcosta@gmail.com>
2025-06-23 19:18:48 +0100

    product entity created

--- services/product-service/src/main/java/com/fred/ecommerce/product_service/entity/Product.java
+++ services/product-service/src/main/java/com/fred/ecommerce/product_service/entity/Product.java
@@ -1,4 +1,27 @@
 package com.fred.ecommerce.product_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String imageURL;
 }
