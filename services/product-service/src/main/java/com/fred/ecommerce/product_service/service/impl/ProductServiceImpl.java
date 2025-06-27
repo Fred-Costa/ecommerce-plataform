fb3baee69c701a3aebd7f7e205fd2e180b1b63fd
Frederico Costa <Fredericobrasilcosta@gmail.com>
2025-06-27 16:55:16 +0100

    impl of some methods

--- services/product-service/src/main/java/com/fred/ecommerce/product_service/service/impl/ProductServiceImpl.java
+++ services/product-service/src/main/java/com/fred/ecommerce/product_service/service/impl/ProductServiceImpl.java
@@ -1,12 +1,16 @@
 package com.fred.ecommerce.product_service.service.impl;
 
 import com.fred.ecommerce.product_service.dto.ProductDTO;
+import com.fred.ecommerce.product_service.entity.Product;
+import com.fred.ecommerce.product_service.mapper.ProductMapper;
 import com.fred.ecommerce.product_service.repository.ProductRepository;
 import com.fred.ecommerce.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +18,12 @@ public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
 
+    private ProductMapper mapper;
+
     @Override
     public ProductDTO create(ProductDTO dto) {
-        return null;
+        Product product = mapper.toEntity(dto);
+        return mapper.toDTO(productRepository.save(product));
     }
 
     @Override
@@ -26,7 +33,13 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDTO findById(Long id) {
-        return null;
+        if (!productRepository.existsById(id)) {
+            throw new NoSuchElementException("Product with this id not founded: " + id);
+        }
+
+        Product product = productRepository.findById(id).orElseThrow();
+
+        return mapper.toDTO(product);
     }
 
     @Override
@@ -36,6 +49,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void delete(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new NoSuchElementException("Product with this id not founded: " + id);
+        }
 
+        productRepository.deleteById(id);
     }
 }
